6610ce4bc831972087b3ba480174cc746c705e75
zhill2 <zachary.a.hill@outlook.com>
2024-11-08 20:02:31 -0800

    Added ssh alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -3,3 +3,5 @@ alias ls='eza -l -h --git --all --icons=always'
 alias l2='eza -l -T -L 2 --git --all --icons=always'
 alias l3='eza -l -T -L 3 --git --all --icons=always'
 
+alias ssh='kitty +kitten ssh'
+
